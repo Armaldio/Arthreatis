373a04a0e9dce86352942f378af859425409666d
Quentin Goinaud <armaldio@gmail.com>
2024-04-16 06:55:37 +0000

    add vercel vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    vercel(),
   ],
+  vercel: {
+    // optional configuration options, see "Advanced usage" below for details
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
