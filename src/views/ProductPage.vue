583f31976150421e869ab666f6abd4d1051882d6
Quentin Goinaud <quentin.goinaud@gmail.com>
2024-04-30 09:07:37 +0200

    use fr products

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -275,7 +275,7 @@ onMounted(async () => {
   console.log("result", { anonymouseSignInData, error });
 
   const dataRaw = await fetch(
-    "https://world.openfoodfacts.net/api/v2/product/" +
+    "https://fr.openfoodfacts.net/api/v2/product/" +
       code +
       "?cc=fr&lc=fr&tags_lc=fr"
   );
